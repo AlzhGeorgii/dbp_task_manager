51a7bf35565fa2f8027653224fdfa6c36b72b0c2
Georgii Alzhirov <alzh.george@gmail.com>
2023-05-31 19:01:54 +0000

    added test_admin

--- main/models/task.py
+++ main/models/task.py
@@ -22,13 +22,13 @@ class Task(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField()
-    date_created = models.DateTimeField()
-    date_updated = models.DateTimeField()
-    date_finish = models.DateTimeField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_finish = models.DateTimeField(null=True)
     state = models.CharField(max_length=255, default=State.NEW, choices=State.choices)
     priority = models.IntegerField(default=Priority.LOW, choices=Priority.choices)
     tags = models.ManyToManyField(Tag, related_name="tasks")
-    task_creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_tasks")
+    task_creator = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="created_tasks")
     task_performer = models.ForeignKey(
         User, null=True, blank=True, on_delete=models.SET_NULL, related_name="assigned_tasks"
     )
