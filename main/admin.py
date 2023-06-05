179f74cbaac3610a5bb6fd59266f31608963a537
Georgii Alzh <124917697+AlzhGeorgii@users.noreply.github.com>
2023-06-05 20:45:17 +0300

    Feature/add main app (#4)
    * add VS Code files to .gitignore
    
    * added main app
    
    * added task and tag models
    
    * added task and tag to admin panel
    
    * added test_admin
    
    * reformat code using black

---
+++ main/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from .models import Tag, Task, User
+
+
+class TaskManagerAdminSite(admin.AdminSite):
+    pass
+
+
+task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
+
+
+@admin.register(User, site=task_manager_admin_site)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "email", "is_staff", "role")
+
+
+@admin.register(Tag, site=task_manager_admin_site)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task, site=task_manager_admin_site)
+class TaskAdmin(admin.ModelAdmin):
+    pass
---
+++ main/models/task.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from .tag import Tag
+from .user import User
+
+
+class Task(models.Model):
+    class State(models.TextChoices):
+        NEW = "new task"
+        IN_DEVELOPMENT = "in development"
+        IN_QA = "in qa"
+        IN_CODE_REVIEW = "in code review"
+        READY_FOR_RELEASE = "ready for release"
+        RELEASED = "released"
+        ARCHIVED = "archived"
+
+    class Priority(models.IntegerChoices):
+        LOW = 0
+        MEDIUM = 1
+        HIGH = 2
+        CRITICAL = 3
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_finish = models.DateTimeField(null=True)
+    state = models.CharField(max_length=255, default=State.NEW, choices=State.choices)
+    priority = models.IntegerField(default=Priority.LOW, choices=Priority.choices)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+    task_creator = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="created_tasks")
+    task_performer = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.SET_NULL, related_name="assigned_tasks"
+    )
+
+    def __str__(self):
+        return f"Task '{self.title}', state: {self.state}, id: {self.id}"
